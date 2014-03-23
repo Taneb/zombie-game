dd06bf629cb57b64c3c012f5db5b6c98f29e6a6d
Nathan van Doorn <nvd1234@gmail.com>
2014-03-23 06:33:23 +0000

    Added moving target

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,9 @@
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE FlexibleContexts #-}
 module Main where
 
 import Control.Lens
+import Control.Monad.State
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as M
 import FreeGame
@@ -32,14 +34,14 @@ type GameMap = Map Coord (Either StaticEntity Zombie)
 
 data GameState = GameState {
   _gameMap :: GameMap,
-  _target :: Position,
+  _target :: Coord,
   _ticker :: Int -- ticker `elem` [0,16)
   }
 makeLenses ''GameState
 
 drawStaticEntity :: Picture2D p => Coord -> StaticEntity -> p ()
 drawStaticEntity coord Wall =
-  color green $ polygon $ map ((*) 12 . fmap fromIntegral)
+  color blue $ polygon $ map ((*) 12 . fmap fromIntegral)
   [coord, coord & _x +~ 1, coord & traverse +~ 1, coord & _y +~ 1]
 
 drawZombie :: Picture2D p => Bitmap -> Int -> Zombie -> p ()
@@ -49,11 +51,16 @@ drawZombie zombieSprite t Zombie {_position = p} =
       pos = p ^.(start.to (fmap fromIntegral)) + (fromIntegral t / 16) *^ dvec
   in rotateR angle $ translate (12*(pos+0.5)) $ bitmap zombieSprite
 
+drawTarget :: Picture2D p => Coord -> p ()
+drawTarget c = color green $ translate (12 * (fmap fromIntegral c + 0.5)) $
+               circleOutline 6
+
 drawGame :: (Monad p, Picture2D p) => Bitmap -> GameState -> p ()
 drawGame zombieSprite gameState = do
   imapMOf_ (gameMap . ifolded <. _Left) drawStaticEntity gameState
   let drawZombie1 = drawZombie zombieSprite $ gameState ^. ticker
   mapMOf_ (gameMap . folded . _Right) drawZombie1 gameState
+  drawTarget $ gameState ^. target
 
 loadMap :: FilePath -> IO GameMap
 loadMap path = do
@@ -69,10 +76,23 @@ loadMap path = do
       Just $ Right Zombie {_position = Transit {_start = c, _dest = c}}
     convert _ _ = Nothing
 
+targetPos :: (Applicative f, Mouse f, Local f) => f Coord
+targetPos = fmap floor . flip (/) 12 <$> mousePosition
+
+gameFrame ::
+  (MonadState GameState f, Applicative f, Mouse f, Local f, Picture2D f) =>
+  Bitmap -> f ()
+gameFrame zombieSprite = do
+  target <~ targetPos
+  s <- get
+  drawGame zombieSprite s
+  return ()
+
 main :: IO ()
 main = do
   m <- loadMap "data/map1.map"
   z <- readBitmap "data/zombie.png"
-  _ <- runGame Windowed (BoundingBox 0 0 600 600) $ foreverFrame $ drawGame z $
-    GameState {_gameMap = m, _target = undefined, _ticker = 0}
+  let initState = GameState {_gameMap = m, _target = V2 0 0, _ticker = 0}
+      gameFrame1 = execStateT (gameFrame z) initState
+  _ <- runGame Windowed (BoundingBox 0 0 600 600) $ foreverFrame gameFrame1
   return ()
